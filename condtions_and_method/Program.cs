953e354c89a6d390f88675555507ecda3e052855
Abdallah Mustapha <abdollahsuper@gmail.com>
2022-10-08 19:57:13 +0000

    Roadmap to c#

---
+++ condtions_and_method/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace condtions_and_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int squareValue = 20;
+            int? result;
+            Console.WriteLine("please enter you square value");
+            squareValue = int.Parse(Console.ReadLine());
+            result = square(squareValue);
+            Console.WriteLine(result);
+            plusTwo(20.0);
+        }
+
+        static int square(int i)
+        {
+            int value = i;
+            return value * value;
+        }
+
+         static void plusTwo(double anyNumber)
+        {
+            int x = 2;
+            double result = (anyNumber + x);
+            Console.WriteLine("+2 value: "+result);
+        }
+
+        }
+}
---
+++ step_1_cSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace step_1_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            int age;
+            double myResult = 10.20; 
+            Console.WriteLine(myResult);
+            Console.WriteLine("Enter your result");
+
+            myResult = double.Parse(Console.ReadLine());
+            Console.WriteLine(myResult);
+            Console.WriteLine((float)myResult);
+            Console.WriteLine("Name");
+            name = Console.ReadLine();
+            Console.WriteLine("age");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine(name + " "+ age);
+        }
+    }
+}
