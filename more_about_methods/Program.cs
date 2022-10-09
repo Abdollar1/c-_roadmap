bd6f1ece3ba206582980f6eadbfcea28e8afdbfb
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2022-10-09 09:54:27 +0000

    passing by swaping implemented

--- more_about_methods/Program.cs
+++ more_about_methods/Program.cs
@@ -2,11 +2,63 @@
 
 namespace more_about_methods
 {
-    class Program
+    class program
     {
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            student Std = new student();
+            Std.fullName("asa", "dsf");
+            Console.WriteLine(Std.getFullName);
+            teacher tc = new teacher("asap", "dasty");
+            Console.WriteLine(tc.getFullName);
+
         }
+    }
+
+
+    class student
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public void fullName(string fn, string ln)
+        {
+            this.firstName = fn;
+            this.lastName = ln;
+
+            Console.WriteLine(fn + ln);
+        }
+
+        public virtual String getFullName
+        {
+            get
+            {
+                return "Student Full name:  {0}:" + this.firstName + " " + this.lastName;
+            }
+        }
+    }
+
+    class teacher : student
+    {
+        public teacher(string fn, string ln)
+        {
+
+            this.firstName = fn;
+            this.lastName = ln;
+        }
+        public override String getFullName
+        {
+            get
+            {
+                return "Teacher Full name:  {0}:" + this.firstName + " " + this.lastName;
+
+            }
+
+        }
+
+
     }
 }
---
+++ passing_parameters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace passing_parameters
+{
+    class Program
+    {
+      
+
+        public static void Main( )
+        {
+          int firstValue = 10;
+        Console.WriteLine("the assigned value: "+ firstValue);
+            insert(ref firstValue);
+
+            Console.WriteLine("the main value: " +  firstValue);
+
+            int i = 2; int j = 3;
+           Console.WriteLine("i = {0}:" + i + " " + "j = {0}:" + j);
+            swap(ref i, ref j);
+            Console.WriteLine("i = {0}:" + i + " " + "j = {0}:" + j);
+        }
+
+         static void insert(ref int value)
+        {
+           value = 100;
+            Console.WriteLine("the next value was: " + value);
+            return;
+        }
+
+        static void swap(ref int x,ref int y)
+        {
+            int myValue = x;
+            x = y;
+            y = myValue;
+        }
+
+        }
+}
