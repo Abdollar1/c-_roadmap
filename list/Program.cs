5ee6feabde1891dc4816a9a0ac2b6a84ddae3bb9
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2022-10-09 22:47:01 +0000

    list added

--- list/Program.cs
+++ list/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace list
 {
@@ -6,7 +7,37 @@ namespace list
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> Parts = new List<string>();
+            List<string> newList = new List<string>();
+
+            Parts.Add("Name: Asap");
+            Parts.Add("Name: osman");
+            Parts.Add("Name: abass");
+            Parts.Add("Name: innorik");
+            Parts.Add("Name: pat");
+            Parts.Add("Name: mat");
+            Parts.Add("Name: jes");
+            Parts.Add("Name: jos");
+
+            foreach (var part in Parts)
+            {
+                Console.WriteLine(part);
+
+                newList.Add(part);
+                newList.Sort();
+               
+
+            };
+
+            foreach (var lis in newList)
+            {
+                Console.WriteLine($"{"new List: "+lis}");
+                }
+                   
         }
-    }
+  }
+
+   
 }
+
+ 
